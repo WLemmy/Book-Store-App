bf76a89b290a918cb19ca0c534efab4477f4d245
WLemmy <wilfred.lemayian@gmail.com>
2024-05-13 00:16:16 +0300

    adding the homepage wording

--- src/components/PageHeading.jsx
+++ src/components/PageHeading.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const PageHeading = ({ text }) => {
+  // Split the text into an array of words
+  const words = text.split(' ');
+
   return (
     <div className="container mx-auto max-w-7xl py-8 px-4">
-      <h1 className="text-3xl font-bold">{text}</h1>
+      <h1 className="text-3xl font-bold">
+        {/* Map through the words and apply styles to specific ones */}
+        {words.map((word, index) => (
+          <span
+            key={index}
+            className={word === 'JSBookHub:' ? 'text-orange font-bold' : ''}
+          >
+            {word}{' '}
+          </span>
+        ))}
+      </h1>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,18 +7,24 @@ const Home = () => {
     <>
       <Navbar />
       
-      <div className="bg-slate-500">
+      <div className="relative bg-slate-500">
         <video muted autoPlay className="w-full">
           <source type="video/mp4" src={VideoBackground} />
         </video>
-      </div>
-
-      <PageHeading text="JSBookHub: Your Ultimate Destination for JavaScript Learning" />
-      
-      <div className="container mx-auto max-w-7xl py-8 px-4">
-        <p className="text-lg text-gray-700 mt-4">
-          Are you looking to enhance your JavaScript skills and become a proficient developer? Look no further than JSBookHub – your ultimate destination for JavaScript learning! Whether you're a beginner taking your first steps into the world of coding or an experienced developer aiming to sharpen your skills, JSBookHub has everything you need to succeed.
-        </p>
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center flex-col text-center">
+          <PageHeading text=" JSBookHub: Your Ultimate Destination for JavaScript Learning" /> 
+          <p className="text-lg text-white mt-4">
+          <span style={{ color: 'orange', fontWeight: 'bold' }}> Are you looking to enhance your JavaScript skills and become a proficient developer? 
+            <br />
+            Look no further than JSBookHub – your ultimate destination for JavaScript learning! 
+            <br />
+            Whether you're a beginner taking your first steps into the world of coding or an 
+            <br />
+            experienced developer aiming to sharpen your skills, JSBookHub has everything 
+            <br />
+            you need to succeed.</span>
+          </p>
+        </div>
       </div>
     </>
   );
