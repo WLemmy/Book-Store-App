ba5e06dc51f2a81d92f8f3a0991cdd2a11995d20
WLemmy <wilfred.lemayian@gmail.com>
2024-05-12 20:04:49 +0300

    Stylying the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,32 @@
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-slate-900 text-white">
-        <div className="container mx-auto max-w-7xl flex gap-5 justify-between">
-            <div className="p-3">
-                <Link to = "/">Home</Link>
-            </div>
-
-            <div> 
-                <ul className="flex gap-5 p-3">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/all-books">All Books</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/favorites">Favorites</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/about-us">About Us</Link>
-                    </li>
-                </ul>
-            </div>
+      <div className="container mx-auto max-w-7xl flex justify-between items-center py-3 px-4">
+        <div>
+          <Link to="/" className="text-xl font-bold">The JavaScript BookStore</Link>
         </div>
+        <div>
+          <ul className="flex gap-5">
+            <li>
+              <Link to="/" className="hover:text-gray-300">Home</Link>
+            </li>
+            <li>
+              <Link to="/all-books" className="hover:text-gray-300">All Books</Link>
+            </li>
+            <li>
+              <Link to="/favorites" className="hover:text-gray-300">Favorites</Link>
+            </li>
+            <li>
+              <Link to="/about-us" className="hover:text-gray-300">About Us</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-    
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
