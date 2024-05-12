c95c72dafbcf72e700acedacb2c554e27bddd4fc
WLemmy <wilfred.lemayian@gmail.com>
2024-05-12 23:17:52 +0300

    customizing the home page

--- src/components/PageHeading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PageHeading = ({text}) => {
-  return (
-    <h1 className="text-center my-10 text-4xl text-bold">{text}</h1>
-  )
-}
-
-export default PageHeading
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,27 @@
-import { PageHeading, Navbar } from "../components";
-
-import VideoBackground from "./../assets/video-background.mp4";
+import React from 'react';
+import { Navbar, PageHeading } from '../components';
+import VideoBackground from './../assets/video-background.mp4';
 
 const Home = () => {
   return (
     <>
-        <Navbar />
+      <Navbar />
+      
+      <div className="bg-slate-500">
+        <video muted autoPlay className="w-full">
+          <source type="video/mp4" src={VideoBackground} />
+        </video>
+      </div>
 
-        
-            <div className=" bg-slate-500">
-              <video muted autoPlay={true} className="w-full">
-                <source type="video/mp4" src={VideoBackground} />
-              </video>
-            </div>
-
-            <p></p>
-
+      <PageHeading text="JSBookHub: Your Ultimate Destination for JavaScript Learning" />
+      
+      <div className="container mx-auto max-w-7xl py-8 px-4">
+        <p className="text-lg text-gray-700 mt-4">
+          Are you looking to enhance your JavaScript skills and become a proficient developer? Look no further than JSBookHub – your ultimate destination for JavaScript learning! Whether you're a beginner taking your first steps into the world of coding or an experienced developer aiming to sharpen your skills, JSBookHub has everything you need to succeed.
+        </p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
